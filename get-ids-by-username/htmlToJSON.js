daed4f790e3062c180326a7daea4ccd12dce2315
hackerhgl <hackerhgl@gmail.com>
2023-08-04 18:10:41 +0500

    HTML TO JSON Working

--- get-ids-by-username/htmlToJSON.js
+++ get-ids-by-username/htmlToJSON.js
@@ -1,12 +1,26 @@
 const {parse} = require('node-html-parser');
+const fs = require('fs');
 
 function main() {
     try {
-        const test = parse('<div id="hello"><li>1</li><li>2</li><li>3</li></div>');
-        const x = test.childNodes[0].childNodes.map((x) => x.rawText);
+        const html = fs.readFileSync('./tmp/likes-dump.html', 'utf8');
+        const parsed = parse(html);
+        const links = [];
+        const pages = [];
+        parsed.querySelectorAll('a').forEach((node) => {
+            const link = node.getAttribute('href');
+            const username = link.split(".com/")[1];
 
+            if (username.includes("pages/")) {
+                pages.push(link)
+            } else {
+                links.push({ link, username });
+            }
+        });
 
-        console.log(x);
+        fs.writeFileSync('./data/likes.json', JSON.stringify(links, null, 2));
+        fs.writeFileSync('./data/pages.json', JSON.stringify(pages, null, 2));
+
     } catch (e) {
         console.log("HTML-TO-JSON FAILED");
         console.error(e);
